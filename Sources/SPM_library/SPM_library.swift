0cd17393a13807475ed8f37059524aea6fbb6f01
Slava Gubar <sgubar@mackiev.com>
2018-05-11 14:52:45 +0300

    Add @objc for property declaration

--- Sources/SPM_library/SPM_library.swift
+++ Sources/SPM_library/SPM_library.swift
@@ -1,6 +1,6 @@
 import Foundation
 @objc public class SPM_library : NSObject {
-   public var text = "Hello, World!"
+   @objc public var text = "Hello, World!"
 	
 	@objc public func getText()->String{
 		return self.text
