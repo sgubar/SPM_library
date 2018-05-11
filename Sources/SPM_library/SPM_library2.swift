2fb0a9b130233d2344d2f0f5fd1644034a9eedbf
Slava Gubar <sgubar@mackiev.com>
2018-05-11 12:52:29 +0300

    Added additonal module

---
+++ Sources/SPM_library/SPM_library2.swift
@@ -0,0 +1,15 @@
+//
+//  SPM_library2.swift
+//  SPM_library
+//
+//  Created by Slava Gubar on 5/11/18.
+//
+
+import Foundation
+
+public struct SPM_library2{
+	public init(){
+	
+	}
+	public var text2 = "Hello from SPMLibrary2"
+}
